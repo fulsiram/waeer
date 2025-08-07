8d7784d2dd4e301332d331b9f384e1ccfd2ceec5
fulsiram <git@fulsiram.dev>
2025-08-07 00:27:48 -0400

    Add base methods

--- app/services/weather_providers/base.rb
+++ app/services/weather_providers/base.rb
@@ -2,5 +2,12 @@ module WeatherProviders
   class BadLocationError < Exception; end
 
   class Base
+    def get_current_weather(location)
+      raise NotImplementedError
+    end
+
+    def get_weather_forecast(location)
+      raise NotImplementedError
+    end
   end
 end
